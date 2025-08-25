b10dc50d9c41f2abedaa281500ea465a8c592ecc
Tomáš Raček <tom@krab1k.net>
2025-08-25 09:42:46 +0200

    Target net452

--- Web/WebChemistry.Web/Models/AccountModels.cs
+++ Web/WebChemistry.Web/Models/AccountModels.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Web.Mvc;
 
 namespace WebChemistry.Web.Models
 {
